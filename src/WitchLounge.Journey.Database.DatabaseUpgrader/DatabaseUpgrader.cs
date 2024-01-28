39d28685a50916efd10510918841347a306c93cc
Caitlin McCann <toomanycats@witchlounge.ca>
2024-01-28 14:49:02 -0800

    Rename application projects and sort out proper namespacing

---
+++ src/WitchLounge.Journey.Database.DatabaseUpgrader/DatabaseUpgrader.cs
@@ -0,0 +1,8 @@
+﻿
+namespace WitchLounge.Journey.Database {
+  internal class DatabaseUpgrader {
+    static void Main(string[] args){
+      Console.WriteLine("Hello World");      
+    }    
+  }
+}
---
+++ src/WitchLounge.Journey.Database/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using DbUp;
+
+namespace WitchLounge.Journey.Database;
+
+public class Class1
+{
+  public void method1 () {
+    var connectionString = "Server=localhost; Username=postgres; Password=postgres";
+
+      var upgrader = DeployChanges.To
+      .PostgresqlDatabase(connectionString)
+      .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+      .LogToConsole()
+      .Build();
+      
+      var result = upgrader.PerformUpgrade();
+  }
+}
