63dc6995e867caa4d4f1257c81ebcd7e008a7386
Caitlin McCann <toomanycats@witchlounge.ca>
2024-01-28 17:52:38 -0800

    convert dbupgrader to generic host, add Serilog

--- src/WitchLounge.Journey.Database.DatabaseUpgrader/DatabaseUpgrader.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace WitchLounge.Journey.Database {
-  internal class DatabaseUpgrader {
-    static void Main(string[] args){
-      Console.WriteLine("Hello World");      
-    }    
-  }
-}
---
+++ src/WitchLounge.Journey.Database.DatabaseUpgrader/DatabaseUpgraderService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WitchLounge.Journey.Database.DatabaseUpgrader;
+
+internal sealed class DatabaseUpgraderService(ILogger<DatabaseUpgraderService> logger, IConfiguration configuration, IHostApplicationLifetime hostApplicationLifetime) : IHostedService {
+  private readonly ILogger _logger = logger; 
+  private readonly IHostApplicationLifetime _hostApplicationLifetime = hostApplicationLifetime;
+  private readonly IConfiguration _configuration = configuration;
+
+    public Task StartAsync(CancellationToken cancellationToken){    
+    _hostApplicationLifetime.ApplicationStarted.Register(() => Task.Run(async () =>
+    {
+        try
+        {
+            _logger.LogInformation("Hello World");
+            await Task.Delay(1000);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Unhandled exception");
+        }
+        finally
+        {
+            _hostApplicationLifetime.StopApplication();
+        }
+    }));
+    return Task.CompletedTask;    
+  }
+
+  public Task StopAsync(CancellationToken cancellationToken) {
+    return Task.CompletedTask;
+  }
+}
